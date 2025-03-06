948f3529f6ab2397971b3c1ac6c9b9ab46f1f065
Andrew Vittiglio <andrewv433@gmail.com>
2025-03-05 23:41:17 -0500

    final new changes

--- components/Work/resume-constants.ts
+++ components/Work/resume-constants.ts
@@ -5,7 +5,7 @@ export const resume: Project[] = [
     title: 'Download',
     src: 'resume.png',
     color: '#dbeafe',
-    url: 'https://drive.google.com/file/d/1lFjmy5VwPaZku1RwiKEw2ITkDkQWZc2p/view',
+    url: 'https://drive.google.com/file/d/1KzwrLxIH8tzbU4kB_IvQhZT3h1z5scM5/view?usp=sharing',
     role: 'Updated June 19th, 2024',
   }
 ]
